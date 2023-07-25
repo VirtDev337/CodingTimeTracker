dbae956ef64937e2e63e593b5de30dcd14fe60e0
VirtDev337 <337harvey@gmail.com>
2023-07-25 14:37:26 -0600

    Organizing classes, add functionality

--- code_time.py
+++ code_time.py
@@ -1,5 +1,5 @@
 import argparse
-import pkg.codetime as codetime
+import pkg.trackers as trackers
 import pkg.codetime_gui as gui
 import attempts.reports
 import psutil as util
@@ -8,17 +8,17 @@ from typing import List, Dict
 
 
 # Initialize CodeTime instance
-codetime = codetime.CodeTimeTracker()
+trackers = trackers.CodeTimeTracker()
 
 # Define command line arguments
 parser = argparse.ArgumentParser(
     description='Track coding projects and browser usage'
 )
 parser.add_argument(
-    '-p',
-    '--project',
+    '-c',
+    '--complete',
     type=str,
-    help='Generate PDF report for project name'
+    help='Toggles projects status for active tracking.'
 )
 parser.add_argument(
     '-d',
@@ -32,19 +32,47 @@ parser.add_argument(
     type=str,
     help='Generate PDF report for date/time (YYYY-MM-DD or HH:MM)'
 )
+parser.add_argument(
+    '-p',
+    '--project',
+    type=str,
+    help='Generate PDF report for project name'
+)
+parser.add_argument(
+    '-r',
+    '--remove',
+    type=str,
+    help='Used in conjuction with "-p" to remove all data associated with the project specified.'
+)
+parser.add_argument(
+    '-s',
+    '--status',
+    type=str,
+    help='Used in conjuction with "-p" to display the status associated with the project specified.'
+)
+parser.add_argument(
+    '-v',
+    '--verbose',
+    type=str,
+    help='Used in conjuction with "-p" to display the status associated with the project specified.'
+)
 args = parser.parse_args()
 
 if __name__ == '__main__':
     if len(args) > 0:
         if "help" in args or "h" in args:
-            codetime.help()
-        elif 'project' in args or 'p' in args and 'date' in args or 'd' in args:
-            codetime.date_project_report(args['date'], args['project'])
-        elif 'date' in args or 'd' in args:
-            codetime.date_report(args['date'])
-        elif 'project' in args or 'p' in args:
-            codetime.project_report(args['project'])
+            trackers.help()
 
-        exit()
+        elif 'project' in args or 'p' in args and 'date' in args or 'd' in args and not 'r' in args or not 'remove' in args and not 's' in args or not 'status' in args:
+            trackers.date_project_report(args['date'], args['project'])
+
+        elif 'date' in args or 'd' in args:
+            trackers.date_report(args['date'])
+
+        elif 'project' in args or 'p' in args:
+            trackers.project_report(args['project'])
+
+        trackers.start()
+
     else:
-        codetime.start()
+        pass
---
+++ pkg/project.py
@@ -0,0 +1,38 @@
+import os
+from datetime import date, datetime
+
+
+class Project:
+    def __init__(self, name):
+        self.name = name
+        self.modified_files = set()
+        self.time_spent = 0
+        self.date = date.today()
+        self.last_modified_date = ""
+        self.created = ""
+        self.complete = False
+        self.dir = os.path.basename(os.getcwd())
+
+    def add_modified_file(self, path):
+        self.modified_files.add(path)
+
+    def add_time_spent(self, seconds):
+        self.time_spent += seconds
+
+    def update_last_modified(self):
+        self.last_modified_date = self.date
+
+    def update_created(self):
+        if not self.created:
+            self.created = self.date
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'directory': self.dir,
+            'modified_files': list(self.modified_files),
+            'time_spent': self.time_spent,
+            'last_modified_date': self.last_modified_date,
+            'created': self.created,
+            'complete': self.complete
+        }
